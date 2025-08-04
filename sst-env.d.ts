47df91b7010a53984494f24e5f2d72523c0c7fe6
Yuan Huang <huangyuan3h@gmail.com>
2025-08-04 21:47:17 +0800

    Remove MarketData and MyLinkableValue type definitions from sst-env.d.ts and sst.pyi files to streamline resource interface.

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -5,14 +5,6 @@
 
 declare module "sst" {
   export interface Resource {
-    "MarketData": {
-      "name": string
-      "type": "sst.aws.Dynamo"
-    }
-    "MyLinkableValue": {
-      "foo": string
-      "type": "sst.sst.Linkable"
-    }
   }
 }
 /// <reference path="sst-env.d.ts" />
